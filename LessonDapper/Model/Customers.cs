8aa72eb9b593fe9fed59ec309ee2ca20cc93447b
RamziNazarov <Ramzi.13.09.18@gmail.com>
2020-05-16 10:35:08 +0300

    create and end HW

---
+++ LessonDapper/Model/Customers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LessonDapper
+{
+    class Customers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ LessonDapper/Repositories/CustomersRepositories/CustomersRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LessonDapper
+{
+    class CustomersRepository : RepositoryBase<Customers>
+    {
+        public CustomersRepository()
+            :base()
+        {
+
+        }
+
+    }
+}
